49cee8cb225de7b1d1c9bcb2597ad8247933b1c7
verdox <mail.ysp@web.de>
2025-04-07 20:45:14 +0000

    Adding many methods to MCCCustomBlockType.

--- api/src/main/java/de/verdox/mccreativelab/custom/block/MCCCustomBlockType.java
+++ api/src/main/java/de/verdox/mccreativelab/custom/block/MCCCustomBlockType.java
@@ -3,37 +3,47 @@ package de.verdox.mccreativelab.custom.block;
 import de.verdox.mccreativelab.custom.annotation.MCCCustomType;
 import de.verdox.mccreativelab.wrapper.block.MCCBlockState;
 import de.verdox.mccreativelab.wrapper.block.MCCBlockType;
+import de.verdox.mccreativelab.wrapper.entity.MCCEntity;
 import de.verdox.mccreativelab.wrapper.entity.types.MCCPlayer;
 import de.verdox.mccreativelab.wrapper.item.MCCItemStack;
 import de.verdox.mccreativelab.wrapper.world.MCCLocation;
 import de.verdox.mccreativelab.wrapper.world.level.biome.MCCBiome;
 
+import java.util.List;
+
 @MCCCustomType
-public abstract class MCCCustomBlockType implements MCCBlockType {
+public interface MCCCustomBlockType extends MCCBlockType {
+    void spawnAfterBreak(MCCBlockState state, MCCLocation location, MCCItemStack stack, boolean dropExperience);
 
-    public MCCCustomBlockType() {
+    void attack(MCCBlockState state, MCCLocation location, MCCPlayer player);
 
-    }
+    void tick(MCCBlockState state, MCCLocation location, RandomSource random);
 
-    public abstract void spawnAfterBreak(MCCBlockState state, MCCLocation location, MCCItemStack stack, boolean dropExperience);
+    void randomTick(MCCBlockState state, MCCLocation location, RandomSource random);
 
-    public abstract void attack(MCCBlockState state, MCCLocation location, MCCPlayer player);
+    boolean canSurvive(MCCBlockState state, MCCLocation location);
 
-    public abstract void tick(MCCBlockState state, MCCLocation location, RandomSource random);
+    MCCBlockState rotate(MCCBlockState state, float rotation);
 
-    public abstract void randomTick(MCCBlockState state, MCCLocation location, RandomSource random);
+    //TODO: Mixin at ServerLevel;tickPrecipitation
+    void handlePrecipitation(MCCBlockState state, MCCLocation location, MCCBiome.Precipitation precipitation);
 
-    public abstract boolean canSurvive(MCCBlockState state, MCCLocation location);
+    //TODO: Mixin at Entity;applyEffectsFromBlocks
+    void stepOn(MCCLocation location, MCCBlockState state, MCCEntity entity);
 
-    public abstract MCCBlockState rotate(MCCBlockState state, float rotation);
+    //TODO: Mixin at ServerPlayerGameMode;destroyBlock -> Paper deprecated the playerDestroy method so on paper we might want to use BlockBreakEvent for this?
+    void destroy(MCCLocation location, MCCBlockState state);
 
-    public abstract boolean dropFromExplosion(Explosion explosion);
+    void onProjectileHit(MCCBlockState state, MCCLocation location, BlockHitResult hit, MCCProjectile projectile);
 
-    public abstract void handlePrecipitation(MCCBlockState state, MCCLocation location, MCCBiome.Precipitation precipitation);
+    List<MCCItemStack> getDrops(MCCBlockState state, LootParams.Builder params);
 
-    public abstract void stepOn(MCCLocation location, MCCBlockState state, Entity entity);
+    MenuProvider getMenuProvider(MCCBlockState state, MCCLocation location);
 
-    public abstract void wasExploded(MCCLocation location, Explosion explosion);
+    InteractionResult useItemOn(MCCItemStack stack, MCCBlockState state, MCCLocation location, MCCPlayer player, InteractionHand hand, BlockHitResult hitResult);
 
-    public abstract void destroy(MCCLocation location, MCCBlockState state);
+    InteractionResult useWithoutItem(MCCBlockState state, MCCLocation location, MCCPlayer player, BlockHitResult hitResult);
+
+    @Override
+    MCCCustomBlockState getDefaultState();
 }
