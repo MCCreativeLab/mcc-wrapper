37895f740dba113cdaa83edb9f40bd9fc0e29c4a
verdox <mail.ysp@web.de>
2025-04-13 13:20:48 +0000

    Implement NMSGameFactory

--- vanilla/src/main/java/de/verdox/mccreativelab/impl/vanilla/custom/NMSGameFactory.java
+++ vanilla/src/main/java/de/verdox/mccreativelab/impl/vanilla/custom/NMSGameFactory.java
@@ -1,13 +1,20 @@
 package de.verdox.mccreativelab.impl.vanilla.custom;
 
 import de.verdox.mccreativelab.custom.MCCGameFactory;
+import de.verdox.mccreativelab.custom.item.MCCCustomItemType;
+import de.verdox.mccreativelab.wrapper.item.MCCItemStack;
 import de.verdox.mccreativelab.wrapper.platform.MCCPlatform;
+import de.verdox.mccreativelab.wrapper.typed.MCCDataComponentTypes;
+import net.kyori.adventure.key.Key;
+
+import java.util.Optional;
 
 public class NMSGameFactory implements MCCGameFactory {
     private final MCCPlatform platform;
 
     public NMSGameFactory(MCCPlatform platform) {
         this.platform = platform;
+        createRegistries();
     }
 
     private void createRegistries() {
@@ -16,5 +23,16 @@ public class NMSGameFactory implements MCCGameFactory {
         platform.getRegistryStorage().createMinecraftRegistry(MCCGameFactory.ITEM_REGISTRY.key());
         platform.getRegistryStorage().createMinecraftRegistry(MCCGameFactory.POI_TYPE_REGISTRY.key());
         platform.getRegistryStorage().createMinecraftRegistry(MCCGameFactory.VILLAGER_PROFESSION_REGISTRY.key());
+    }
+
+    @Override
+    public Optional<MCCCustomItemType> extract(MCCItemStack mccItemStack) {
+        Key itemModelKey = mccItemStack.components().get(MCCDataComponentTypes.ITEM_MODEL.get());
+        if (itemModelKey == null) {
+            return Optional.empty();
+        } else if (itemModelKey.namespace().equals("minecraft")) {
+            return Optional.empty();
+        }
+        return MCCGameFactory.ITEM_REGISTRY.get().getOptional(itemModelKey);
     }
 }
