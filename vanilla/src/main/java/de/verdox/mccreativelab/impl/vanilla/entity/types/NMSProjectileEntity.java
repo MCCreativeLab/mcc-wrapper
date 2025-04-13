1d59e04ec056d1c67f3e8934f5dc01cc9fd2ec36
verdox <mail.ysp@web.de>
2025-04-13 13:20:36 +0000

    Add NMSProjectileEntity

---
+++ vanilla/src/main/java/de/verdox/mccreativelab/impl/vanilla/entity/types/NMSProjectileEntity.java
@@ -0,0 +1,32 @@
+package de.verdox.mccreativelab.impl.vanilla.entity.types;
+
+import de.verdox.mccreativelab.conversion.converter.MCCConverter;
+import de.verdox.mccreativelab.impl.vanilla.entity.NMSEntity;
+import de.verdox.mccreativelab.wrapper.entity.MCCEntity;
+import de.verdox.mccreativelab.wrapper.entity.types.MCCProjectileEntity;
+import de.verdox.mccreativelab.wrapper.platform.MCCHandle;
+import net.minecraft.world.entity.projectile.Projectile;
+import org.jetbrains.annotations.Nullable;
+
+public class NMSProjectileEntity extends NMSEntity<Projectile> implements MCCProjectileEntity {
+    public static final MCCConverter<Projectile, NMSProjectileEntity> CONVERTER = MCCHandle.converter(NMSProjectileEntity.class, Projectile.class, NMSProjectileEntity::new, MCCHandle::getHandle);
+
+    public NMSProjectileEntity(Projectile handle) {
+        super(handle);
+    }
+
+    @Override
+    public void setOwner(@Nullable MCCEntity owner) {
+        handle.setOwner(conversionService.unwrap(owner));
+    }
+
+    @Override
+    public @Nullable MCCEntity getOwner() {
+        return conversionService.wrap(handle.getOwner());
+    }
+
+    @Override
+    public @Nullable MCCEntity getEffectSource() {
+        return conversionService.wrap(handle.getEffectSource());
+    }
+}
