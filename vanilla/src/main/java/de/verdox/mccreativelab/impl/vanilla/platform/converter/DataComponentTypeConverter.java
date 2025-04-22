11086dd59d12fb5e60624cdd06ae6bc4ff32c642
verdox <mail.ysp@web.de>
2025-04-22 20:31:43 +0000

    Implement changes and add serializers

--- vanilla/src/main/java/de/verdox/mccreativelab/impl/vanilla/platform/converter/DataComponentTypeConverter.java
+++ vanilla/src/main/java/de/verdox/mccreativelab/impl/vanilla/platform/converter/DataComponentTypeConverter.java
@@ -5,8 +5,10 @@ import de.verdox.mccreativelab.impl.vanilla.item.components.NMSCustomModelData;
 
 import java.lang.Boolean;
 
+import de.verdox.mccreativelab.wrapper.MCCKeyedWrapper;
 import de.verdox.mccreativelab.wrapper.item.components.*;
-import net.minecraft.world.item.component.BundleContents;
+import de.verdox.mccreativelab.wrapper.platform.serialization.MCCSerializers;
+import de.verdox.vserializer.generic.Serializer;
 import net.minecraft.world.level.saveddata.maps.MapId;
 import de.verdox.mccreativelab.impl.vanilla.item.components.NMSMapItemColor;
 import net.minecraft.world.food.FoodProperties;
@@ -15,13 +17,10 @@ import de.verdox.mccreativelab.impl.vanilla.item.components.NMSEnchantable;
 import de.verdox.mccreativelab.impl.vanilla.item.components.NMSDyedItemColor;
 import net.minecraft.world.item.component.SuspiciousStewEffects;
 import de.verdox.mccreativelab.impl.vanilla.item.components.NMSLodestoneTracker;
-import net.minecraft.world.item.DyeColor;
-import net.minecraft.world.level.block.entity.BeehiveBlockEntity;
 import de.verdox.mccreativelab.impl.vanilla.item.components.NMSUseCooldown;
 import net.minecraft.core.Holder;
 import de.verdox.mccreativelab.conversion.converter.MCCConverter;
 import de.verdox.mccreativelab.impl.vanilla.item.components.NMSDataComponentType;
-import net.kyori.adventure.key.Key;
 
 import java.util.List;
 
@@ -30,15 +29,10 @@ import net.minecraft.world.item.component.MapItemColor;
 import java.lang.Class;
 
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.component.CustomData;
 import net.minecraft.core.component.DataComponents;
 import de.verdox.mccreativelab.wrapper.registry.MCCTypedKey;
-import net.minecraft.world.item.component.Fireworks;
-import net.minecraft.world.level.block.entity.PotDecorations;
-import net.minecraft.world.item.component.WrittenBookContent;
 import net.minecraft.world.item.enchantment.Repairable;
 import de.verdox.mccreativelab.impl.vanilla.item.components.NMSSuspiciousStewEffects;
-import net.minecraft.world.item.equipment.trim.ArmorTrim;
 import de.verdox.mccreativelab.impl.vanilla.item.components.NMSTool;
 import de.verdox.mccreativelab.impl.vanilla.item.components.NMSMapId;
 import net.minecraft.core.component.DataComponentType;
@@ -46,30 +40,22 @@ import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.component.UseRemainder;
 import net.minecraft.world.item.component.ChargedProjectiles;
 import de.verdox.mccreativelab.wrapper.registry.MCCReference;
-import net.minecraft.world.item.component.OminousBottleAmplifier;
 import net.minecraft.world.item.component.LodestoneTracker;
 import de.verdox.mccreativelab.impl.vanilla.item.components.NMSRepairable;
 import de.verdox.mccreativelab.impl.vanilla.item.components.NMSBlockItemStateProperties;
 import net.minecraft.world.item.component.Tool;
 import de.verdox.mccreativelab.impl.vanilla.item.components.NMSChargedProjectiles;
-import net.minecraft.world.item.AdventureModePredicate;
 import net.minecraft.world.item.component.DyedItemColor;
 import net.minecraft.world.item.JukeboxPlayable;
 import de.verdox.mccreativelab.impl.vanilla.item.components.NMSJukeboxPlayable;
-import net.minecraft.world.item.Rarity;
 import de.verdox.mccreativelab.impl.vanilla.item.components.NMSUnbreakable;
 import net.minecraft.world.item.enchantment.Enchantable;
-import net.minecraft.world.item.component.ItemContainerContents;
 import net.minecraft.world.item.component.CustomModelData;
 import de.verdox.mccreativelab.impl.vanilla.item.components.NMSItemLore;
 import net.minecraft.world.item.component.UseCooldown;
-import net.minecraft.world.item.component.DebugStickState;
 import net.minecraft.world.item.component.ItemLore;
-import net.minecraft.world.level.block.entity.BannerPatternLayers;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.item.component.WritableBookContent;
-import net.minecraft.world.item.enchantment.ItemEnchantments;
 import net.minecraft.world.item.component.BlockItemStateProperties;
 
 import java.lang.Integer;
@@ -106,91 +92,91 @@ public class DataComponentTypeConverter implements MCCConverter<DataComponentTyp
 
     public MCCConverter.ConversionResult<NMSDataComponentType> wrap(DataComponentType nativeType) {
         if (nativeType.equals(DataComponents.MAX_STACK_SIZE)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Integer>() {}, new TypeToken<Integer>() {}, null));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Integer>() {}, new TypeToken<>() {}, Serializer.Primitive.INTEGER));
         }
         if (nativeType.equals(DataComponents.MAX_DAMAGE)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Integer>() {}, new TypeToken<Integer>() {}, null));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Integer>() {}, new TypeToken<>() {}, Serializer.Primitive.INTEGER));
         }
         if (nativeType.equals(DataComponents.DAMAGE)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Integer>() {}, new TypeToken<Integer>() {}, null));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Integer>() {}, new TypeToken<>() {}, Serializer.Primitive.INTEGER));
         }
         if (nativeType.equals(DataComponents.UNBREAKABLE)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Unbreakable>() {}, new TypeToken<MCCUnbreakable>() {}, () -> new NMSUnbreakable(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Unbreakable>() {}, new TypeToken<>() {}, MCCUnbreakable.SERIALIZER, () -> new NMSUnbreakable(null)));
         }
         if (nativeType.equals(DataComponents.CUSTOM_NAME)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Component>() {}, new TypeToken<net.kyori.adventure.text.Component>() {}, null));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Component>() {}, new TypeToken<>() {}, MCCSerializers.COMPONENT));
         }
         if (nativeType.equals(DataComponents.ITEM_NAME)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Component>() {}, new TypeToken<net.kyori.adventure.text.Component>() {}, null));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Component>() {}, new TypeToken<>() {}, MCCSerializers.COMPONENT));
         }
         if (nativeType.equals(DataComponents.ITEM_MODEL)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<ResourceLocation>() {}, new TypeToken<Key>() {}, null));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<ResourceLocation>() {}, new TypeToken<>() {}, MCCKeyedWrapper.KEY_SERIALIZER));
         }
         if (nativeType.equals(DataComponents.LORE)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<ItemLore>() {}, new TypeToken<MCCItemLore>() {}, () -> new NMSItemLore(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<ItemLore>() {}, new TypeToken<MCCItemLore>() {}, null, () -> new NMSItemLore(null)));
         }
         if (nativeType.equals(DataComponents.CUSTOM_MODEL_DATA)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<CustomModelData>() {}, new TypeToken<MCCCustomModelData>() {}, () -> new NMSCustomModelData(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<CustomModelData>() {}, new TypeToken<MCCCustomModelData>() {}, null, () -> new NMSCustomModelData(null)));
         }
         if (nativeType.equals(DataComponents.REPAIR_COST)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Integer>() {}, new TypeToken<Integer>() {}, null));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Integer>() {}, new TypeToken<>() {}, Serializer.Primitive.INTEGER));
         }
         if (nativeType.equals(DataComponents.ENCHANTMENT_GLINT_OVERRIDE)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Boolean>() {}, new TypeToken<Boolean>() {}, null));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Boolean>() {}, new TypeToken<>() {}, Serializer.Primitive.BOOLEAN));
         }
         if (nativeType.equals(DataComponents.FOOD)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<FoodProperties>() {}, new TypeToken<MCCFoodProperties>() {}, () -> new NMSFoodProperties(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<FoodProperties>() {}, new TypeToken<MCCFoodProperties>() {}, null, () -> new NMSFoodProperties(null)));
         }
         if (nativeType.equals(DataComponents.USE_REMAINDER)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<UseRemainder>() {}, new TypeToken<MCCUseRemainder>() {}, () -> new NMSUseRemainder(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<UseRemainder>() {}, new TypeToken<MCCUseRemainder>() {}, null, () -> new NMSUseRemainder(null)));
         }
         if (nativeType.equals(DataComponents.USE_COOLDOWN)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<UseCooldown>() {}, new TypeToken<MCCUseCooldown>() {}, () -> new NMSUseCooldown(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<UseCooldown>() {}, new TypeToken<MCCUseCooldown>() {}, null, () -> new NMSUseCooldown(null)));
         }
         if (nativeType.equals(DataComponents.TOOL)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Tool>() {}, new TypeToken<MCCTool>() {}, () -> new NMSTool(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Tool>() {}, new TypeToken<MCCTool>() {}, null, () -> new NMSTool(null)));
         }
         if (nativeType.equals(DataComponents.ENCHANTABLE)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Enchantable>() {}, new TypeToken<MCCEnchantable>() {}, () -> new NMSEnchantable(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Enchantable>() {}, new TypeToken<MCCEnchantable>() {}, null, () -> new NMSEnchantable(null)));
         }
         if (nativeType.equals(DataComponents.REPAIRABLE)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Repairable>() {}, new TypeToken<MCCRepairable>() {}, () -> new NMSRepairable(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<Repairable>() {}, new TypeToken<MCCRepairable>() {}, null, () -> new NMSRepairable(null)));
         }
         if (nativeType.equals(DataComponents.TOOLTIP_STYLE)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<ResourceLocation>() {}, new TypeToken<Key>() {}, null));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<ResourceLocation>() {}, new TypeToken<>() {}, MCCKeyedWrapper.KEY_SERIALIZER));
         }
         if (nativeType.equals(DataComponents.DYED_COLOR)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<DyedItemColor>() {}, new TypeToken<MCCDyedItemColor>() {}, () -> new NMSDyedItemColor(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<DyedItemColor>() {}, new TypeToken<MCCDyedItemColor>() {}, null, () -> new NMSDyedItemColor(null)));
         }
         if (nativeType.equals(DataComponents.MAP_COLOR)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<MapItemColor>() {}, new TypeToken<MCCMapItemColor>() {}, () -> new NMSMapItemColor(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<MapItemColor>() {}, new TypeToken<MCCMapItemColor>() {}, null, () -> new NMSMapItemColor(null)));
         }
         if (nativeType.equals(DataComponents.MAP_ID)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<MapId>() {}, new TypeToken<MCCMapId>() {}, () -> new NMSMapId(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<MapId>() {}, new TypeToken<MCCMapId>() {}, null, () -> new NMSMapId(null)));
         }
         if (nativeType.equals(DataComponents.CHARGED_PROJECTILES)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<ChargedProjectiles>() {}, new TypeToken<MCCChargedProjectiles>() {}, () -> new NMSChargedProjectiles(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<ChargedProjectiles>() {}, new TypeToken<MCCChargedProjectiles>() {}, null, () -> new NMSChargedProjectiles(null)));
         }
         if (nativeType.equals(DataComponents.SUSPICIOUS_STEW_EFFECTS)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<SuspiciousStewEffects>() {}, new TypeToken<MCCSuspiciousStewEffects>() {}, () -> new NMSSuspiciousStewEffects(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<SuspiciousStewEffects>() {}, new TypeToken<MCCSuspiciousStewEffects>() {}, null, () -> new NMSSuspiciousStewEffects(null)));
         }
         if (nativeType.equals(DataComponents.INSTRUMENT)) {
             return done(new NMSDataComponentType<>(nativeType, new TypeToken<Holder<Instrument>>() {}, new TypeToken<MCCReference<MCCInstrument>>() {}, null));
         }
         if (nativeType.equals(DataComponents.JUKEBOX_PLAYABLE)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<JukeboxPlayable>() {}, new TypeToken<MCCJukeboxPlayable>() {}, () -> new NMSJukeboxPlayable(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<JukeboxPlayable>() {}, new TypeToken<MCCJukeboxPlayable>() {}, null, () -> new NMSJukeboxPlayable(null)));
         }
         if (nativeType.equals(DataComponents.RECIPES)) {
             return done(new NMSDataComponentType<>(nativeType, new TypeToken<List<ResourceKey<Recipe<?>>>>() {}, new TypeToken<List<MCCTypedKey<Recipe<?>>>>() {}, null));
         }
         if (nativeType.equals(DataComponents.LODESTONE_TRACKER)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<LodestoneTracker>() {}, new TypeToken<MCCLodestoneTracker>() {}, () -> new NMSLodestoneTracker(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<LodestoneTracker>() {}, new TypeToken<MCCLodestoneTracker>() {}, null, () -> new NMSLodestoneTracker(null)));
         }
         if (nativeType.equals(DataComponents.NOTE_BLOCK_SOUND)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<ResourceLocation>() {}, new TypeToken<Key>() {}, null));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<ResourceLocation>() {}, new TypeToken<>() {}, MCCKeyedWrapper.KEY_SERIALIZER));
         }
         if (nativeType.equals(DataComponents.BLOCK_STATE)) {
-            return done(new NMSDataComponentType<>(nativeType, new TypeToken<BlockItemStateProperties>() {}, new TypeToken<MCCBlockItemStateProperties>() {}, () -> new NMSBlockItemStateProperties(null)));
+            return done(new NMSDataComponentType<>(nativeType, new TypeToken<BlockItemStateProperties>() {}, new TypeToken<MCCBlockItemStateProperties>() {}, null, () -> new NMSBlockItemStateProperties(null)));
         }
         return notDone(null);
     }
