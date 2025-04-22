abed8439b35ddec5f2f45f7f3e78834cc6f2b9e4
verdox <mail.ysp@web.de>
2025-04-22 20:30:32 +0000

    Add value serializer to data component type

--- vanilla/src/main/java/de/verdox/mccreativelab/impl/vanilla/item/components/NMSDataComponentType.java
+++ vanilla/src/main/java/de/verdox/mccreativelab/impl/vanilla/item/components/NMSDataComponentType.java
@@ -3,6 +3,7 @@ package de.verdox.mccreativelab.impl.vanilla.item.components;
 import com.google.common.reflect.TypeToken;
 import de.verdox.mccreativelab.wrapper.item.components.MCCDataComponentType;
 import de.verdox.mccreativelab.wrapper.platform.MCCHandle;
+import de.verdox.vserializer.generic.Serializer;
 import net.minecraft.core.component.DataComponentType;
 
 import java.util.function.Supplier;
@@ -11,12 +12,18 @@ public class NMSDataComponentType<N, T, I extends T> extends MCCHandle<DataCompo
     private final TypeToken<N> nativeDataClass;
     private final TypeToken<T> apiDataClass;
     private final Supplier<I> defaultCreator;
+    private final Serializer<T> apiSerializer;
 
-    public NMSDataComponentType(DataComponentType<N> handle, TypeToken<N> nativeDataClass, TypeToken<T> apiDataClass, Supplier<I> defaultCreator) {
+    public NMSDataComponentType(DataComponentType<N> handle, TypeToken<N> nativeDataClass, TypeToken<T> apiDataClass, Serializer<T> apiSerializer, Supplier<I> defaultCreator) {
         super(handle);
         this.nativeDataClass = nativeDataClass;
         this.apiDataClass = apiDataClass;
         this.defaultCreator = defaultCreator;
+        this.apiSerializer = apiSerializer;
+    }
+
+    public NMSDataComponentType(DataComponentType<N> handle, TypeToken<N> nativeDataClass, TypeToken<T> apiDataClass, Serializer<T> apiSerializer) {
+        this(handle, nativeDataClass, apiDataClass, apiSerializer, null);
     }
 
     @Override
@@ -34,5 +41,10 @@ public class NMSDataComponentType<N, T, I extends T> extends MCCHandle<DataCompo
         if (this.defaultCreator == null)
             throw new IllegalStateException("No default creator available for type " + apiDataClass);
         return defaultCreator.get();
+    }
+
+    @Override
+    public Serializer<T> getValueSerializer() {
+        return apiSerializer;
     }
 }
