8b3aae36a94d9ef8540acb4ec386d8fad989e48a
verdox <mail.ysp@web.de>
2025-04-22 20:18:04 +0000

    Add new methods to MCCDataComponentMap

--- api/src/main/java/de/verdox/mccreativelab/wrapper/item/components/MCCDataComponentMap.java
+++ api/src/main/java/de/verdox/mccreativelab/wrapper/item/components/MCCDataComponentMap.java
@@ -47,7 +47,32 @@ public interface MCCDataComponentMap extends Iterable<MCCDataComponentType<?>> {
      */
     <R, T extends MCCDataComponentType<R>> void set(T dataComponentType, @Nullable R value);
 
+    /**
+     * Sets the stored value of a data component type.
+     *
+     * @param typedDataComponentType the typed component type
+     * @param <R>               the generic data type
+     */
+    default <R> void set(MCCTypedDataComponentType<R> typedDataComponentType) {
+        set(typedDataComponentType.key(), typedDataComponentType.value());
+    }
+
+    /**
+     * Checks if the component map contains a particular type
+     * @param type the type
+     * @return true if exists in the map
+     */
     boolean has(MCCDataComponentType<?> type);
+
+    /**
+     * Stores all key value pairs from a provided map in this map
+     * @param map the other map
+     */
+    default void putAll(MCCDataComponentMap map) {
+        for (MCCTypedDataComponentType<?> mccTypedDataComponentType : map.pairSet()) {
+            set(mccTypedDataComponentType);
+        }
+    }
 
     /**
      * Returns the amount of data components saved in this map
@@ -66,4 +91,10 @@ public interface MCCDataComponentMap extends Iterable<MCCDataComponentType<?>> {
      * @return the key set
      */
     Set<MCCDataComponentType<?>> keySet();
+
+    /**
+     * Returns a set consisting of key value pairs in this map
+     * @return the set
+     */
+    Set<MCCTypedDataComponentType<?>> pairSet();
 }
