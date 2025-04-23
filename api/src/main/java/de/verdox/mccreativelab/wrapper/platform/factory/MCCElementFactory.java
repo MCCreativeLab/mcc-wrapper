d630b1d1a32972ca1bc3e10314838e4759bea316
verdox <mail.ysp@web.de>
2025-04-23 14:32:05 +0000

    Add function to create empty data components

--- api/src/main/java/de/verdox/mccreativelab/wrapper/platform/factory/MCCElementFactory.java
+++ api/src/main/java/de/verdox/mccreativelab/wrapper/platform/factory/MCCElementFactory.java
@@ -1,6 +1,7 @@
 package de.verdox.mccreativelab.wrapper.platform.factory;
 
 import de.verdox.mccreativelab.wrapper.item.components.MCCDataComponentMap;
+import de.verdox.mccreativelab.wrapper.item.components.MCCItemComponent;
 
 /**
  * Used to create minecraft objects
@@ -11,4 +12,12 @@ public interface MCCElementFactory {
      * @return an empty data component map
      */
     MCCDataComponentMap createEmptyDataComponentMap();
+
+    /**
+     * Creates an empty component of a provided data class
+     * @param componentClass the data class
+     * @return the empty component
+     * @param <T> the generic component type
+     */
+    <T extends MCCItemComponent> T createEmptyComponent(Class<T> componentClass);
 }
--- vanilla/src/main/java/de/verdox/mccreativelab/impl/vanilla/platform/factory/NMSElementFactory.java
+++ vanilla/src/main/java/de/verdox/mccreativelab/impl/vanilla/platform/factory/NMSElementFactory.java
@@ -1,13 +1,55 @@
 package de.verdox.mccreativelab.impl.vanilla.platform.factory;
 
-import de.verdox.mccreativelab.wrapper.item.components.MCCDataComponentMap;
-import de.verdox.mccreativelab.wrapper.platform.MCCPlatform;
+import de.verdox.mccreativelab.impl.vanilla.item.components.*;
+import de.verdox.mccreativelab.impl.vanilla.platform.NMSPlatform;
+import de.verdox.mccreativelab.wrapper.item.components.*;
 import de.verdox.mccreativelab.wrapper.platform.factory.MCCElementFactory;
 import net.minecraft.core.component.DataComponentMap;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class NMSElementFactory implements MCCElementFactory {
+    private final NMSPlatform nmsPlatform;
+
+    private final Map<Class<? extends MCCItemComponent>, Supplier<? extends MCCItemComponent>> constructors = new HashMap<>();
+
+    public NMSElementFactory(NMSPlatform nmsPlatform) {
+        this.nmsPlatform = nmsPlatform;
+
+        registerComponentConstructor(MCCUnbreakable.class, () -> new NMSUnbreakable(null));
+        registerComponentConstructor(MCCItemLore.class, () -> new NMSItemLore(null));
+        registerComponentConstructor(MCCCustomModelData.class, () -> new NMSCustomModelData(null));
+        registerComponentConstructor(MCCFoodProperties.class, () -> new NMSFoodProperties(null));
+        registerComponentConstructor(MCCUseRemainder.class, () -> new NMSUseRemainder(null));
+        registerComponentConstructor(MCCUseCooldown.class, () -> new NMSUseCooldown(null));
+        registerComponentConstructor(MCCTool.class, () -> new NMSTool(null));
+        registerComponentConstructor(MCCEnchantable.class, () -> new NMSEnchantable(null));
+        registerComponentConstructor(MCCRepairable.class, () -> new NMSRepairable(null));
+        registerComponentConstructor(MCCDyedItemColor.class, () -> new NMSDyedItemColor(null));
+        registerComponentConstructor(MCCMapItemColor.class, () -> new NMSMapItemColor(null));
+        registerComponentConstructor(MCCMapId.class, () -> new NMSMapId(null));
+        registerComponentConstructor(MCCChargedProjectiles.class, () -> new NMSChargedProjectiles(null));
+        registerComponentConstructor(MCCSuspiciousStewEffects.class, () -> new NMSSuspiciousStewEffects(null));
+        registerComponentConstructor(MCCJukeboxPlayable.class, () -> new NMSJukeboxPlayable(null));
+        registerComponentConstructor(MCCBlockItemStateProperties.class, () -> new NMSBlockItemStateProperties(null));
+    }
+
     @Override
     public MCCDataComponentMap createEmptyDataComponentMap() {
-        return MCCPlatform.getInstance().getConversionService().wrap(DataComponentMap.builder().build());
+        return nmsPlatform.getConversionService().wrap(DataComponentMap.builder().build());
+    }
+
+    @Override
+    public <T extends MCCItemComponent> T createEmptyComponent(Class<T> componentClass) {
+        if (!constructors.containsKey(componentClass)) {
+            throw new IllegalArgumentException("Could not create an empty variant of the data component type " + componentClass.getName());
+        }
+        return componentClass.cast(constructors.get(componentClass).get());
+    }
+
+    private <T extends MCCItemComponent> void registerComponentConstructor(Class<T> componentClass, Supplier<T> emptyConstructor) {
+        constructors.put(componentClass, emptyConstructor);
     }
 }
