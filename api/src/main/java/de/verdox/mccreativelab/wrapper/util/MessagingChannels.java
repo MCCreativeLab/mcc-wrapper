65a9457f4a18759bc385ebc23745271216d6e1d1
verdox <mail.ysp@web.de>
2025-04-30 14:02:28 +0000

    Add 2 new components to player

---
+++ api/src/main/java/de/verdox/mccreativelab/wrapper/util/MessagingChannels.java
@@ -0,0 +1,10 @@
+package de.verdox.mccreativelab.wrapper.util;
+
+import net.kyori.adventure.key.Key;
+
+/**
+ * A collection of known plugin messaging channels
+ */
+public interface MessagingChannels {
+    Key BUNGEE_CORD = Key.key("bungeecord:main");
+}
