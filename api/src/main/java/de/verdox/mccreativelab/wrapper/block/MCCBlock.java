b980970655020e70d01d7305ba13b4388d8c7499
verdox <mail.ysp@web.de>
2025-04-30 14:04:32 +0000

    Add getRelative function to MCCBlock

--- api/src/main/java/de/verdox/mccreativelab/wrapper/block/MCCBlock.java
+++ api/src/main/java/de/verdox/mccreativelab/wrapper/block/MCCBlock.java
@@ -14,6 +14,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Describes an actual block in a minecraft world with a location and a type.
@@ -34,6 +35,13 @@ public final class MCCBlock implements MCCKeyedWrapper, PointBlockAccessor<MCCBl
         }
         this.location = location;
         this.mccChunk = mccChunk;
+    }
+
+    /**
+     * Gets the relative of a block
+     */
+    public CompletableFuture<MCCBlock> getRelative(int relX, int relY, int relZ) {
+        return getWorld().at(x() + relX, y() + relY, z() + relZ, mccBlock -> mccBlock);
     }
 
 
