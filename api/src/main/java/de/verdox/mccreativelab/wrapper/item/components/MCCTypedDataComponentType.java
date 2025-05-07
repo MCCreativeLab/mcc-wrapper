41be0e1136fd2a80d47adb12ec109709714a52c3
verdox <mail.ysp@web.de>
2025-05-07 16:09:28 +0000

    Fix compile issues

--- api/src/main/java/de/verdox/mccreativelab/wrapper/item/components/MCCTypedDataComponentType.java
+++ api/src/main/java/de/verdox/mccreativelab/wrapper/item/components/MCCTypedDataComponentType.java
@@ -1,6 +1,7 @@
 package de.verdox.mccreativelab.wrapper.item.components;
 
 
+import de.verdox.vserializer.exception.SerializationException;
 import de.verdox.vserializer.generic.SerializationContext;
 import de.verdox.vserializer.generic.SerializationElement;
 import org.jetbrains.annotations.Nullable;
@@ -9,7 +10,7 @@ import org.jetbrains.annotations.Nullable;
  * An immutable pair consisting of a data component type and an associated value
  */
 public record MCCTypedDataComponentType<T>(MCCDataComponentType<T> key, @Nullable T value) {
-    public SerializationElement serializeElement(SerializationContext context) {
+    public SerializationElement serializeElement(SerializationContext context) throws SerializationException {
         return key.getValueSerializer().serialize(context, value);
     }
 }
