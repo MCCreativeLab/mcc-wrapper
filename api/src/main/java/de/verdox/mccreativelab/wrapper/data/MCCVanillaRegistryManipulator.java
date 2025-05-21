a496a9e066a371d768cbd516a31de042e079ab13
verdox <mail.ysp@web.de>
2025-05-21 13:37:08 +0000

    Make experimental

--- api/src/main/java/de/verdox/mccreativelab/wrapper/data/MCCVanillaRegistryManipulator.java
+++ api/src/main/java/de/verdox/mccreativelab/wrapper/data/MCCVanillaRegistryManipulator.java
@@ -1,5 +1,7 @@
 package de.verdox.mccreativelab.wrapper.data;
 
+import org.jetbrains.annotations.ApiStatus;
+
 /**
  * Used as a helper class that injects runnable code after the minecraft registries are bootstrapped.
  * We need this because paper has its own registry manipulation logic before the minecraft registries are bootstrapped.
@@ -7,6 +9,8 @@ package de.verdox.mccreativelab.wrapper.data;
  * We need this because when we want to add stuff like custom attributes we must ensure, that they are registered after the mc attributes.
  * Else the attribute ids that are sent via packets are doomed.
  */
+
+@ApiStatus.Experimental
 public interface MCCVanillaRegistryManipulator {
     /**
      * Injects code into the explained code point.
