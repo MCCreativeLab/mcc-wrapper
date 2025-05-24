2e4512d5054933ad542fd7c643cb0e37517e1808
verdox <mail.ysp@web.de>
2025-05-24 17:36:23 +0000

    Use implicit wrap

--- vanilla/src/main/java/de/verdox/mccreativelab/impl/vanilla/registry/NMSRegistryStorage.java
+++ vanilla/src/main/java/de/verdox/mccreativelab/impl/vanilla/registry/NMSRegistryStorage.java
@@ -156,14 +156,14 @@ public class NMSRegistryStorage implements MCCRegistryStorage {
     @Nullable
     private <T> MCCRegistry<T> searchForRegistry(Key registryKey) {
         if (registryKey.equals(Key.key("minecraft", "root"))) {
-            return MCCPlatform.getInstance().getConversionService().wrap(BuiltInRegistries.REGISTRY, new TypeToken<>() {});
+            return MCCPlatform.getInstance().getConversionService().wrap(BuiltInRegistries.REGISTRY);
         }
 
         if (!registryKey.namespace().equals("minecraft")) {
             if (CUSTOM_REGISTRIES.containsKey(registryKey)) {
-                return MCCPlatform.getInstance().getConversionService().wrap(CUSTOM_REGISTRIES.get(registryKey), new TypeToken<>() {});
+                return MCCPlatform.getInstance().getConversionService().wrap(CUSTOM_REGISTRIES.get(registryKey));
             } else if (CUSTOM_OPEN_REGISTRIES.containsKey(registryKey)) {
-                return MCCPlatform.getInstance().getConversionService().wrap(CUSTOM_OPEN_REGISTRIES.get(registryKey), new TypeToken<>() {});
+                return MCCPlatform.getInstance().getConversionService().wrap(CUSTOM_OPEN_REGISTRIES.get(registryKey));
             } else {
                 throw new NoSuchElementException("Could not find the registry " + registryKey);
             }
