d04e747a393f6362721903e68a4c10d2c8467e8b
verdox <mail.ysp@web.de>
2025-05-25 22:44:56 +0000

    Relocate

--- api/src/main/java/de/verdox/mccreativelab/wrapper/item/components/MCCSeededContainerLoot.java
+++ api/src/main/java/de/verdox/mccreativelab/wrapper/item/components/MCCSeededContainerLoot.java
@@ -1,6 +1,6 @@
 package de.verdox.mccreativelab.wrapper.item.components;
 import de.verdox.mccreativelab.wrapper.registry.MCCTypedKey;
-import de.verdox.mccreativelab.wrapper.types.MCCLootTable;
+import de.verdox.mccreativelab.gamefactory.loottable.MCCLootTable;
 
 /**
  * Defines the loot table present in a container item.
