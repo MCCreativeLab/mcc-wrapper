90a150f97eb1e7ecd86605f604e329cb6cf295be
verdox <mail.ysp@web.de>
2025-05-26 12:51:46 +0000

    Fix custom item type extraction

--- vanilla/src/main/java/de/verdox/mccreativelab/impl/vanilla/mixins/ItemStackMixin.java
+++ vanilla/src/main/java/de/verdox/mccreativelab/impl/vanilla/mixins/ItemStackMixin.java
@@ -1,13 +1,14 @@
 package de.verdox.mccreativelab.impl.vanilla.mixins;
 
-import com.google.common.reflect.TypeToken;
+import de.verdox.mccreativelab.gamefactory.MCCGameFactory;
 import de.verdox.mccreativelab.gamefactory.item.MCCCustomItemType;
 import de.verdox.mccreativelab.impl.vanilla.gamefactory.proxy.ProxyItem;
 import de.verdox.mccreativelab.impl.vanilla.util.mixin.ItemStackWithCustomType;
-import de.verdox.mccreativelab.wrapper.item.MCCItemStack;
-import de.verdox.mccreativelab.wrapper.platform.MCCPlatform;
+import net.kyori.adventure.key.Key;
 import net.minecraft.core.component.DataComponentMap;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.component.PatchedDataComponentMap;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -18,10 +19,11 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.Consumable;
+import net.minecraft.world.item.component.CustomData;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
-import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -31,13 +33,14 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Optional;
-
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin implements ItemStackWithCustomType {
 
     @Unique
     private MCCCustomItemType mcc_wrapper$customItemType;
+
+    @Unique
+    private boolean mcc_wrapper$loadedCustomItemType = false;
 
     @Unique
     private Item mcc_wrapper$cachedItem;
@@ -57,6 +60,11 @@ public abstract class ItemStackMixin implements ItemStackWithCustomType {
     @Shadow
     public abstract ItemStack copy();
 
+    @Final
+    @Shadow
+    PatchedDataComponentMap components;
+
+    @Unique
     public ItemStack self() {
         return (ItemStack) (Object) this;
     }
@@ -64,6 +72,22 @@ public abstract class ItemStackMixin implements ItemStackWithCustomType {
     @Unique
     @Override
     public MCCCustomItemType getMcc_wrapper$customItemType() {
+        if (mcc_wrapper$customItemType == null && !mcc_wrapper$loadedCustomItemType) {
+            mcc_wrapper$loadedCustomItemType = true;
+            if (this.components.has(DataComponents.CUSTOM_DATA)) {
+                CompoundTag compoundTag = this.components.get(DataComponents.CUSTOM_DATA).copyTag();
+                if (compoundTag.contains("mcc.customItemType")) {
+                    try {
+                        Key key = Key.key(compoundTag.getString("mcc.customItemType"));
+                        var found = MCCGameFactory.ITEM_REGISTRY.get().getOptional(key).orElse(null);
+                        setMcc_wrapper$customItemType(found);
+                        return found;
+                    } catch (Exception e) {
+                        MCCGameFactory.LOGGER.log(java.util.logging.Level.SEVERE, "Could not extract a custom item type", e);
+                    }
+                }
+            }
+        }
         return mcc_wrapper$customItemType;
     }
 
@@ -71,6 +95,16 @@ public abstract class ItemStackMixin implements ItemStackWithCustomType {
     @Override
     public void setMcc_wrapper$customItemType(MCCCustomItemType mcc_wrapper$customItemType) {
         this.mcc_wrapper$customItemType = mcc_wrapper$customItemType;
+        mcc_wrapper$loadedCustomItemType = true;
+        CustomData data;
+        if (this.components.has(DataComponents.CUSTOM_DATA)) {
+            data = this.components.get(DataComponents.CUSTOM_DATA).update(compoundTag -> compoundTag.putString("mcc.customItemType", mcc_wrapper$customItemType.key().asString()));
+        } else {
+            CompoundTag compoundTag = new CompoundTag();
+            compoundTag.putString("mcc.customItemType", mcc_wrapper$customItemType.key().asString());
+            data = CustomData.of(compoundTag);
+        }
+        this.components.set(DataComponents.CUSTOM_DATA, data);
     }
 
     @Redirect(
